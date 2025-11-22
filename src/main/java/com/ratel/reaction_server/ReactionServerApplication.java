49d1bcd73d1915d9a2417ef6465bf306460d9e14
Francis Kangaty <franciskangaty@gmail.com>
2025-11-22 21:47:43 +0300

    test commit terminal

---
+++ src/main/java/com/ratel/reaction_server/ReactionServerApplication.java
@@ -0,0 +1,13 @@
+package com.ratel.reaction_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactionServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratel/reaction_server/ReactionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratel.reaction_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
